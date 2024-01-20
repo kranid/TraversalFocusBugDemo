9d35f4ec5fad4ae8086506466f2cda67a48bd3f3
Konstantin galiakhmetov <galiakhmetovkv@gmail.com>
2024-01-20 22:22:52 +0300

    some changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui:1.5.4")
+    implementation("androidx.compose.ui:ui:1.6.0-rc01")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
--- app/src/main/java/com/example/traversalfocusbugdemo/MainActivity.kt
+++ app/src/main/java/com/example/traversalfocusbugdemo/MainActivity.kt
@@ -6,65 +6,60 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.isContainer
 import androidx.compose.ui.semantics.isTraversalGroup
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
-import com.example.traversalfocusbugdemo.ui.theme.TraversalFocusBugDemoTheme
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            BreakingFocusContainer()
+    class MainActivity : ComponentActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContent {
+                Box(Modifier.fillMaxSize()) {
+                    BreakingFocusContainer()
+                }
+            }
         }
     }
-}
 
-@Composable
-fun BreakingFocusContainer() {
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Gray)
-    ) {
+    @Composable
+    fun BreakingFocusContainer() {
         Box(
             modifier = Modifier
-                .size(50.dp)
+                .size(100.dp)
                 .background(Color.Green)
+                .semantics { isTraversalGroup = true }
                 .border(2.dp, Color.Red),
-            Alignment.BottomCenter
+            Alignment.Center
         ) {
-            Text("The first element")
+            Text("Element №2")
+            Text("Element №3", Modifier.offset(y = 30.dp))
         }
         Box(
             Modifier
-                .height(50.dp)
-                .width(200.dp)
-                .offset(y=25.dp)
+                .height(40.dp)
+                .width(100.dp)
                 .background(Color.Blue)
                 .border(2.dp, Color.Yellow),
-                    Alignment.BottomCenter
-        ) {
-            Text("hello world!", Modifier.offset(x = 10.dp))
+            Alignment.Center    ) {
+            Text("Element №1")
+        }
+        Box(
+            modifier = Modifier
+                .offset(y =100.dp)
+                .height(50.dp)
+                .width(100.dp)
+                .background(Color.Magenta),
+            Alignment.Center) {
+            Text("Element №4")
         }
     }
-
-}
